f727f4df35c4207855d44675c59f9706139b5599
Aaron Min <min.aaron.min@gmail.com>
2017-03-16 15:50:06 -0700

    Add coinChange, movingAverages, and powerString to repo

---
+++ movingAverages/movingAverages.js
@@ -0,0 +1,14 @@
+/* Write a function that returns the moving k-averages of
+ * an input array in array form.
+ *
+ * Assume integer division
+ *
+ * Example:
+ * k = 3, array = [1, 2, 10, 5, 3]
+ * returns [6, 8, 9] 
+ *
+ */
+
+var movingAverages = function(k, arr) {
+
+};
---
+++ powerString/powerString.js
@@ -0,0 +1,19 @@
+/* Write a function that accepts two strings (x and y) as input
+ * and returns the computation of x ^ y
+ *
+ * Basically, Math.pow but accepts strings as arguments
+ * 
+ * Assume all input x and y are larger than 32 bits
+ *
+ * Assume all inputs are integers in string format ('' + number)
+ * 
+ */
+
+var powerString = function(x, y) {
+
+};
+
+// This test case is just to see if you can go larger than 32 bits
+
+console.assert(powerString('100000', '2') === '10000000000', "Expected 100,000 to the 2nd power to be 10,000,000,000');
+
