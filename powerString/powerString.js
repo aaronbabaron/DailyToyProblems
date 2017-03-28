53496e3e62283a2f97dd4fd249e60d1d1053b6a2
Aaron Min <min.aaron.min@gmail.com>
2017-03-28 14:19:57 -0700

    Add working test for math.pow

--- powerString/powerString.js
+++ powerString/powerString.js
@@ -10,5 +10,4 @@
  */
 
 export default function(x, y) {
-
 };
--- test.js
+++ test.js
@@ -1,25 +1,35 @@
 import 'babel-polyfill'
 import chai from 'chai'
+import sinon from 'sinon'
 import powerString from './powerString/powerString.js'
 import bac from './bullsAndCows/bullsAndCows.js'
 
 var expect = chai.expect;
 
-xdescribe("Power string", () => {
+describe("Power string", () => {
+  it("should not use math.pow", () => {
+    var spy = sinon.spy(Math, 'pow');    
+
+    powerString('2', '2');
+
+    expect(spy.called).to.equal(false);
+
+    Math.pow.restore();
+  });
+
   it("Should work for values under max int", () => {
     expect('' + Math.pow(2, 2)).to.equal(powerString('2', '2'));
     expect('' + Math.pow(12, 12)).to.equal(powerString('12', '12'));
     expect('' + Math.pow(23, 4)).to.equal(powerString('23', '4'));
     expect('' + Math.pow(-3, 4)).to.equal(powerString('-3', '4'));
     expect('' + Math.pow(2, 4)).to.equal(powerString('2', '4'));
+    expect('1000000000000').to.equal(powerString('1000000', '2'));
+    expect('1000000000000').to.equal(powerString('1000000000000', '1'));
   });
 
-  it("Should work for values above max int", () => {
-    expect('1000000000000').to.equal(powerString('1000000', '2'));
-  });
 });
 
-describe("Bulls and cows", () => {
+xdescribe("Bulls and cows", () => {
   it("Should return the proper value for all guesses", () => {
     expect(bac('1023', '0231')).to.equal('0A4B');
     expect(bac('1023', '9876')).to.equal('0A0B');
